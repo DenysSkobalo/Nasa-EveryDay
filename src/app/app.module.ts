774354c27ae171a66c24f7ce76a443ce2f0045e6
Denys <96637006+DenysSkobalo@users.noreply.github.com>
2022-12-21 21:17:21 +0100

    create structure project and install tailwindCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormComponent} from './components/form/form.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ModalComponent} from './components/modal/modal.component';
+import {PostComponent} from './components/post/post.component';
+import {ApplicationComponent} from './components/application/application.component';
+import {HowWorkComponent} from './components/how-work/how-work.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    ModalComponent,
+    PostComponent,
+    ApplicationComponent,
+    HowWorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +29,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
