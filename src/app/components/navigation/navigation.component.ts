d59102a2487497f457cb4de25d83d644569e4233
Denys <96637006+DenysSkobalo@users.noreply.github.com>
2023-01-05 01:30:53 +0100

    responsive home page is ready

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,13 @@
-import {Component, HostBinding, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html'
 })
 export class NavigationComponent {
+  public isVisibleMenu = false;
+
   public switchDarkTheme() {
     document.body.classList.toggle('dark-theme');
   }
-
-  public isVisibleMenu = false;
 }
